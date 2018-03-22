a815c584c68caad099ad5d92886ac397a91a3aef
shriya <shri17ya@gmail.com>
2018-03-23 01:52:15 +0530

    shriya

---
+++ app/src/main/java/com/example/mansi/busezon/SigningUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.mansi.busezon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SigningUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signing_up);
+    }
+}
