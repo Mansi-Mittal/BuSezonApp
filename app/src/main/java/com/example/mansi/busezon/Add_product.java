400b7fd135468aa64ddcd86f9a82ac3e0f44f7d6
aakritijohar29 <aakritijohar29@gmail.com>
2018-03-23 02:10:20 +0530

    aakriti

---
+++ app/src/main/java/com/example/mansi/busezon/Add_product.java
@@ -0,0 +1,14 @@
+package com.example.mansi.busezon;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Add_product extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_product);
+        }
+    }
