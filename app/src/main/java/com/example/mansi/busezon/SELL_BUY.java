b6076e8723881786668e45bec26340e93fe42d7a
shriya <shri17ya@gmail.com>
2018-04-28 21:33:37 +0530

    login with amazon and profile page display added, Shriya

--- app/src/main/java/com/example/mansi/busezon/SELL_BUY.java
+++ app/src/main/java/com/example/mansi/busezon/SELL_BUY.java
@@ -28,6 +28,7 @@ import com.roughike.bottombar.OnTabSelectListener;
 
 public class SELL_BUY extends AppCompatActivity {
     private  dbHelper mDbHelper;
+    private  String User_Id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +40,8 @@ public class SELL_BUY extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+//        Toast.makeText(SELL_BUY.this, "hi!!", Toast.LENGTH_SHORT).show();
 
-        check();
                 TextView BUY= (TextView) findViewById(R.id.buy);
                 BUY.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -72,72 +73,136 @@ public class SELL_BUY extends AppCompatActivity {
                 }
                 else if (tabId == R.id.tab_profile) {
                     Intent i = new Intent(getApplicationContext(), profile_page.class);
+                    if(!User_Id.equals(""))
+                        i.putExtra("UserId",User_Id);
                     startActivity(i);
                 }
 
             }
         });
-
+        check();
             }
     public void sendMessage(View view)
     {
         Intent intent = new Intent(SELL_BUY.this, profile_page.class);
+        intent.putExtra("Id",User_Id);
         startActivity(intent);
     }
 
 
-    private void check()
-    {
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-        //database reference pointing to demo node
+    private void check() {
+        try {
+            DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+            //database reference pointing to demo node
 //        DatabaseReference demoRef = rootRef.child("CI8hvEW0sfZ0oU1GziTpGYPJv2z2");
 //        String value = "User22";
 //        //push creates a unique id in database
 //        demoRef.push().setValue(value);
-        rootRef.addListenerForSingleValueEvent(new ValueEventListener()
-        {
-            String userId=getIntent().getStringExtra("Id");
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                    for(DataSnapshot ds : dataSnapshot.getChildren())
-                    {
-                        if(ds.getKey().equals(userId))
-                        {
+//            Toast.makeText(SELL_BUY.this, "hi!", Toast.LENGTH_SHORT).show();
+            rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                String EmailIntent = getIntent().getStringExtra("Email");
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        String email = ds.child("email").getValue(String.class);
+//                        Toast.makeText(SELL_BUY.this, email + "  " + EmailIntent, Toast.LENGTH_SHORT).show();
+                        if (email.equals(EmailIntent)) {
                             String name = ds.child("name").getValue(String.class);
-                            String email = ds.child("email").getValue(String.class);
-                            String address = ds.child("address").getValue(String.class);
-                            String phoneno = ds.child("phoneno").getValue(String.class);
-                            String password = ds.child("password").getValue(String.class);
-                            Toast.makeText(SELL_BUY.this, email + " " + ds.getKey(), Toast.LENGTH_SHORT).show();
-                            boolean c = CheckIsDataAlreadyInDBorNot(dbContract.userEntry.TABLE_NAME, dbContract.userEntry.COLUMN_number,phoneno);
-                            if (c == false) {
-                                insertUser(userId,name,email,address,phoneno,password);
+                            // String email = ds.child("email").getValue(String.class);
+                            String userId = ds.getKey();
+                            String address = "";
+                            if (ds.hasChild("address") == true)
+                                address = ds.child("address").getValue(String.class);
+                            String phoneno = "";
+                            if (ds.hasChild("phoneno"))
+                                phoneno = ds.child("phoneno").getValue(String.class);
+                            String password = "";
+                            if (ds.hasChild("password"))
+                                password = ds.child("password").getValue(String.class);
+                            User_Id = userId;
+                            //        insertUser(userId,name,email,address,phoneno,password);
+                            //Toast.makeText(SELL_BUY.this, email + " " + ds.getKey(), Toast.LENGTH_SHORT).show();
+                            try
+                            {
+                            int c = CheckIsDataAlreadyInDBorNot(dbContract.userEntry.TABLE_NAME, dbContract.userEntry.COLUMN_EMAIL, email);
+                            if (c == 0) {
+                                insertUser(userId, name, email, address, phoneno, password);
                             }
-                            else {
-                                Toast.makeText(SELL_BUY.this, "user already exists", Toast.LENGTH_SHORT).show();
+                            else if (c == 2) {
+                          //      Toast.makeText(SELL_BUY.this, "user already exists", Toast.LENGTH_SHORT).show();
                                 //no insertion in local database;
                             }
+                            else
+                            {
+                                Toast.makeText(SELL_BUY.this, "user error", Toast.LENGTH_SHORT).show();
+                            }
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Toast.makeText(SELL_BUY.this, "error  :"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                         }
                     }
 
                 }
 
-            @Override
-        public void onCancelled(DatabaseError databaseError) {}
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
 
-        });
-    }
-    public boolean CheckIsDataAlreadyInDBorNot(String TableName, String dbfield, String fieldValue) {
-        mDbHelper = new dbHelper(this);
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        String Query = "Select * from " + TableName + " where " + dbfield + " = " + fieldValue;
-        Cursor cursor = db.rawQuery(Query, null);
-        if(cursor.getCount() <= 0){
-            cursor.close();
-            return false;
+            });
         }
-        cursor.close();
-        return true;
+        catch (Exception e)
+        {
+            Toast.makeText(SELL_BUY.this, "user 1111", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public int CheckIsDataAlreadyInDBorNot(String TableName, String dbfield, String fieldValue)
+    {
+
+    int f=1;
+        try {
+            mDbHelper = new dbHelper(this);
+            SQLiteDatabase db = mDbHelper.getReadableDatabase();
+            String Query = "Select * from " + TableName;
+            Cursor cursor = db.rawQuery(Query, null);
+            //Toast.makeText(SELL_BUY.this, "111", Toast.LENGTH_LONG).show();
+            if (cursor.getCount() <= 0) {
+                f = 0;
+              //  Toast.makeText(SELL_BUY.this, "222", Toast.LENGTH_LONG).show();
+            } else if (cursor.getCount() > 0) {
+                //cursor.close();
+                //return true;
+                //  Toast.makeText(SELL_BUY.this,"333",Toast.LENGTH_LONG).show();
+                if (cursor.moveToFirst()) {
+                    do {
+                //        Toast.makeText(SELL_BUY.this, "444", Toast.LENGTH_LONG).show();
+                        if (cursor.getString(2).equals(fieldValue)) {
+                            f = 2;
+                          //  Toast.makeText(SELL_BUY.this, cursor.getString(2)+" "+ fieldValue, Toast.LENGTH_LONG).show();
+                            break;
+                        }
+                        else
+                            f=0;
+                    } while (cursor.moveToNext());
+                }
+               // cursor.close();
+               // Toast.makeText(SELL_BUY.this, "aaa", Toast.LENGTH_LONG).show();
+            }
+
+          //  Toast.makeText(SELL_BUY.this,"bbbb"+ f, Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(SELL_BUY.this, "error checkDB() "+f, Toast.LENGTH_LONG).show();
+            //Toast.makeText(SELL_BUY.this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+        return f;
+//        return true;
     }
     private void insertUser(String user_Id,String nameString,String EMAILString,String addressString,String numbertString,String pass) {
         // Create database helper
@@ -147,13 +212,12 @@ public class SELL_BUY extends AppCompatActivity {
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-
-        values.put(dbContract.userEntry.COLUMN_USER_ID, user_Id);
-        values.put(dbContract.userEntry.COLUMN_USER, nameString);
-        values.put(dbContract.userEntry.COLUMN_EMAIL, EMAILString);
-        values.put(dbContract.userEntry.COLUMN_number, numbertString);
-        values.put(dbContract.userEntry.COLUMN_address, addressString);
-        values.put(dbContract.userEntry.COLUMN_password, pass);
+            values.put(dbContract.userEntry.COLUMN_USER_ID, user_Id);
+            values.put(dbContract.userEntry.COLUMN_USER, nameString);
+            values.put(dbContract.userEntry.COLUMN_EMAIL, EMAILString);
+            values.put(dbContract.userEntry.COLUMN_number, numbertString);
+            values.put(dbContract.userEntry.COLUMN_address, addressString);
+            values.put(dbContract.userEntry.COLUMN_password, pass);
 
         // Insert a new row for user in the database, returning the ID of that new row.
         long newRowId = db.insert(dbContract.userEntry.TABLE_NAME, null, values);
@@ -162,9 +226,11 @@ public class SELL_BUY extends AppCompatActivity {
         if (newRowId == -1) {
             // If the row ID is -1, then there was an error with insertion.
             Toast.makeText(this, "Error with saving user", Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        else
+            {
             // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, "user saved " + newRowId, Toast.LENGTH_SHORT).show();
+        //    Toast.makeText(this, "user saved " + newRowId, Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/mansi/busezon/profile_page.java
+++ app/src/main/java/com/example/mansi/busezon/profile_page.java
@@ -1,6 +1,8 @@
 package com.example.mansi.busezon;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.IdRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,46 +10,102 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mansi.busezon.data.dbContract;
+import com.example.mansi.busezon.data.dbHelper;
+import com.google.firebase.auth.FirebaseAuth;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class profile_page extends AppCompatActivity {
 
+    private TextView profileName,profileEmail,profilePhoneno,profileAddress;
+    private  dbHelper mDbHelper;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        //getActionBar().setTitle("BuSezon");
-        setSupportActionBar(myToolbar);
+        try {
+            profileName = findViewById(R.id.profile_name);
+            profileEmail = findViewById(R.id.profile_email);
+            profileAddress = findViewById(R.id.profile_address);
+            profilePhoneno = findViewById(R.id.profile_number);
+            firebaseAuth = FirebaseAuth.getInstance();
+            PutValuesInFields();
 
-        ActionBar ab = getSupportActionBar();
+            Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+            //getActionBar().setTitle("BuSezon");
+            setSupportActionBar(myToolbar);
 
-        ab.setDisplayHomeAsUpEnabled(true);
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_wishList) {
-                    Intent i=new Intent(getApplicationContext(),WishlistActivity.class);
+            ActionBar ab = getSupportActionBar();
+
+            ab.setDisplayHomeAsUpEnabled(true);
+            BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+            bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+                @Override
+                public void onTabSelected(@IdRes int tabId) {
+                    if (tabId == R.id.tab_wishList) {
+                        Intent i = new Intent(getApplicationContext(), WishlistActivity.class);
+                        startActivity(i);
+                    } else if (tabId == R.id.tab_profile) {
+                        Intent i = new Intent(getApplicationContext(), profile_page.class);
+                        startActivity(i);
+                    }
+
+                }
+            });
+            Button Wishlist = (Button) findViewById(R.id.wishlist);
+            Wishlist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    Intent i = new Intent(getApplicationContext(), WishlistActivity.class);
                     startActivity(i);
                 }
-                else if (tabId == R.id.tab_profile) {
-                    Intent i = new Intent(getApplicationContext(), profile_page.class);
-                    startActivity(i);
-                }
-
-            }
-        });
-        Button Wishlist=(Button)findViewById(R.id.wishlist);
-        Wishlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent i = new Intent(getApplicationContext(), WishlistActivity.class);
-                startActivity(i);
-            }
-        });
+            });
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(profile_page.this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
     }
+    void PutValuesInFields() {
+        mDbHelper = new dbHelper(this);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        String Query = "Select * from " + dbContract.userEntry.TABLE_NAME;
+        Cursor cursor = db.rawQuery(Query, null);
+        if (cursor.getCount() <= 0) {
+            cursor.close();
+            Toast.makeText(profile_page.this, "no", Toast.LENGTH_LONG).show();
+        } else
+            {
+//            String userId = firebaseAuth.getCurrentUser().getUid();
+               String userId = getIntent().getStringExtra("Id");
+       //         Toast.makeText(profile_page.this, userId, Toast.LENGTH_LONG).show();
+            String name = "", email = "", phoneno = "", address = "", bio = "";
 
+            if (cursor.moveToFirst()) {
+                do {
+                    String id = cursor.getString(0);
+                    if (id.equals(userId)) {
+                       // Toast.makeText(profile_page.this,id,Toast.LENGTH_LONG).show();
+                        name = cursor.getString(1);
+                        email = cursor.getString(2);
+                        phoneno = cursor.getString(3);
+                        address = cursor.getString(5);
+                        break;
+                    }
+
+                } while (cursor.moveToNext());
+            }
+            profileName.setText(name);
+            profileEmail.setText(email);
+            profilePhoneno.setText(phoneno);
+            profileAddress.setText(address);
+
+            cursor.close();
+        }
+    }
 }
