063ba4c64594a5f452b083faaff8ce295cd296e0
aakritijohar29 <aakritijohar29@gmail.com>
2018-05-01 23:40:16 +0530

    adapter view added-aakriti

--- app/src/main/java/com/example/mansi/busezon/SellHomepage.java
+++ app/src/main/java/com/example/mansi/busezon/SellHomepage.java
@@ -1,34 +1,19 @@
 package com.example.mansi.busezon;
 
 import android.content.Intent;
-import android.media.Image;
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.EventLog;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
-import android.os.AsyncTask;
-import java.net.URL;
-import android.util.Log;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class SellHomepage extends AppCompatActivity {
 
@@ -44,15 +29,35 @@ public class SellHomepage extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
 
         ab.setDisplayHomeAsUpEnabled(true);
+        ArrayList<offers> offersList=new ArrayList<>();
+        //offersList.add(new offers(R.drawable.img1,"Great discounts"));
+        offersList.add(new offers(R.drawable.pdt1,""));
+        offersList.add(new offers(R.drawable.pdt2,""));
+        offersList.add(new offers(R.drawable.pdt3,""));
+        offersList.add(new offers(R.drawable.pdt4,""));
+        offersList.add(new offers(R.drawable.pdt5,""));
+        offersList.add(new offers(R.drawable.pdt6,""));
+        offersList.add(new offers(R.drawable.pdt7,""));
+        offersList.add(new offers(R.drawable.pdt8,""));
 
-        ImageView SellPdt1=(ImageView) findViewById(R.id.sellpdt1);
+        ListView offersListView = (ListView) findViewById(R.id.list1);
+
+        // Create a new {@link ArrayAdapter} of earthquakes
+        final offersAdapter adapter = new offersAdapter(this, offersList);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        offersListView.setAdapter(adapter);
+
+
+       /* ImageView SellPdt1=(ImageView) findViewById(R.id.sellpdt1);
         SellPdt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Intent i = new Intent(getApplicationContext(), ProductDescription.class);
                 startActivity(i);
             }
-        });
+        });*/
         Button Prod=(Button)findViewById(R.id.addBtn);
         Prod.setOnClickListener(new View.OnClickListener() {
             @Override
